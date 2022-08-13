ebf14536c3d8a710ba9a4a8a9de4f6ca8ff8b3f2
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-08-13 17:11:24 +0800

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int mile;
+    double km;
+    
+    scanf("%d",&mile);
+    km=(double)num*1.6;
+    printf("%.1f\n",km);
+    
+    return 0;
+}
